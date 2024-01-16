74579bc9b33b0a5a4c9c0403e59bae646d7ab49d
Grzegorz Krason <grzegorz.krason@gmail.com>
2024-01-16 22:57:25 +0100

    add context manager

--- README.md
+++ README.md
@@ -8,3 +8,29 @@ from stdcpp import cout, cerr, endl
 cout << "Hello" << " " << "world!" << endl
 cerr << "Hello" << " " << "world!" << endl
 ```
+
+Bonus 1:
+
+```py
+from stdcpp import Stream
+
+s = Stream('log.txt', 'a+')
+s << "Hello "
+s.close()
+
+with Stream('log.txt', 'a+') as s:
+    s << "world!" << endl
+```
+
+Bonus 2:
+
+```py
+import io
+from stdcpp import Stream, cout
+
+ios = io.StringIO()
+s = Stream(ios)
+s << "Hello world!" << endl
+ios.seek(0)
+cout << ios.read()
+```
--- stdcpp/__init__.py
+++ stdcpp/__init__.py
@@ -1,11 +1,14 @@
 import sys
+from pathlib import Path
 
 class StreamHandler:
     def handle(self, stream):
         pass
 
 class Stream:
-    def __init__(self, fh):
+    def __init__(self, fh, *args, **kwargs):
+        if isinstance(fh, (str, Path)):
+            fh = open(str(fh), *args, **kwargs)
         self.fh = fh
 
     def __lshift__(self, other):
@@ -13,6 +16,15 @@ class Stream:
             return other.handle(self)
         self.fh.write(str(other))
         return self
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, *_):
+        self.close()
+        
+    def close(self):
+        self.fh.close()
 
 class Endl(StreamHandler):
     def handle(self, stream):
